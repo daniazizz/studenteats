d6fb2a8e0d6e47877fa85b9ceb1d701c604b1f96
MemexVUB <58112783+MemexVUB@users.noreply.github.com>
2019-12-14 05:23:53 +0100

    Added the model for the restaurant. Also, the marker showing the user location has been made differentiable from those who are showing a eating place location.

--- mapservice/views.py
+++ mapservice/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from mapservice.models import EatingPlace
 
 # Create your views here.
 @login_required
 def map(request):
-    return render(request, 'mapservice/map.html', {'title': 'Map'})
+    restaurants = EatingPlace.objects.all()
+    return render(request, 'mapservice/map.html', {'title': 'Map', 'restaurants' : restaurants})
